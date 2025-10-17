d3bc79302999e4be3ea8b5a6056e8d6e2cc2bc6f
MAGS <120091142+MAGS-GH@users.noreply.github.com>
2025-10-17 09:41:48 +0200

    Update CORS

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -16,8 +16,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFlutterApp", policy =>
     {
         policy.WithOrigins(
-                "https://h4-demo.mercantec.tech",
-                "https://h4-demo-api.mercantec.tech"  
+                "https://h4-flutter.mercantec.tech",
+                "https://h4-api.mercantec.tech"  
             )
             .AllowAnyMethod()               // Allow GET, POST, PUT, DELETE, etc.
             .AllowAnyHeader()               // Allow any headers
